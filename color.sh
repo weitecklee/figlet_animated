1a9e74e43da1d0eeb22e167db52778f67fefc2c0
arkteck <arkteck@gmail.com>
2024-08-09 10:33:05 -0400

    refactor to not clear terminal

--- color.sh
+++ color.sh
@@ -7,28 +7,38 @@ fi
 
 DELAY=$(echo "scale=4; 1 / $FPS" | bc)
 TEMP_FILE=$(mktemp)
-figlet -c -f /usr/share/fonts/3d.flf $* > "$TEMP_FILE"
+figlet -c -f /usr/share/fonts/3d.flf "$*" > "$TEMP_FILE"
 
-clear
+if [ $? -ne 0 ]; then
+  rm -f "$TEMP_FILE"
+  exit 1
+fi
+
+LINE_COUNT=$(wc -l < "$TEMP_FILE")
 
 printf "\033[?25l" # hide cursor
 
 cleanup() {
   rm -f "$TEMP_FILE"
+  # Make sure cursor is below graphic
+  printf "\033[%dB\033[G" "$LINE_COUNT"
   printf "\033[?25h" # show cursor
   exit
 }
 
-trap cleanup INT TERM
+trap cleanup INT TERM EXIT
 
 i=0
 
 while true; do
-  printf "\033[H" # move cursor to (0, 0)
   lolcat -f -o "$i" "$TEMP_FILE"
   ((i++))
   if [ "$i" -gt 509 ]; then
     i=0
   fi
+
+  # Move cursor up by number of lines in TEMP_FILE then to beginning of current line
+  printf "\033[%dA\033[G" "$LINE_COUNT"
+
   sleep $DELAY
 done
